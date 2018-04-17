343f9cd343497992f7c6d5abde4d4814a38ab190
Mriyam Tamuli <mbtamuli@gmail.com>
2018-04-17 21:04:16 +0530

    Add macos_setup script
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

---
+++ macos_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "WARNING: EasyEngine v4 is currently in beta."
+echo "Setting up things to install EasyEngine. This might take some time."
+curl -s https://php-osx.liip.ch/install.sh | bash -s 7.2
+echo "Getting latest EasyEngine phar"
+curl -o /usr/local/bin/easyengine.phar https://raw.githubusercontent.com/EasyEngine/easyengine-builds/master/phar/easyengine-nightly.phar
+
+cat > /usr/local/bin/ee <<EOF
+#!/bin/bash
+
+/usr/local/php5/bin/php /usr/local/bin/easyengine.phar "$@"
+EOF
+
+if command -v docker > /dev/null 2>&1; then
+	echo "You don't have Docker installed. Please install Docker for Mac"
+	echo "https://docs.docker.com/docker-for-mac/install/"
+else
+  images=( "base" "nginx-proxy" "nginx" "php" "mariadb" "phpmyadmin" "mail" "redis" )
+	for image in "${images[@]}" ; do
+		echo "Getting $image image"
+		docker pull easyengine/"$image"
+	done
+fi
