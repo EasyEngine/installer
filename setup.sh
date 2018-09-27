533ef6f7bceb1cea68324f2438fe8be420712d02
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-09-27 12:47:49 +0530

    Add export of proper locale for Ubuntu and debian
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- setup.sh
+++ setup.sh
@@ -29,6 +29,14 @@ function setup_docker() {
 
 function setup_php() {
     # Checking linux distro. Currently only Ubuntu and Debian are supported.
+
+    if [[ ("$ee_linux_distro" == "Ubuntu" ) || ("$ee_linux_distro" == "Debian") ]]; then
+        # Adding locales as there is language related issue with ondrej repository.
+        apt-get install apt-transport-https lsb-release ca-certificates locales locales-all -y
+        export LC_ALL=en_US.UTF-8
+        export LANG=en_US.UTF-8
+    fi
+
     if [ "$ee_linux_distro" == "Ubuntu" ]; then
       echo "Installing PHP cli"
       # Adding software-properties-common for add-apt-repository.
@@ -40,10 +48,6 @@ function setup_php() {
       apt-get -y install php7.2-cli
     elif [ "$ee_linux_distro" == "Debian" ]; then
       echo "Installing PHP cli"
-      # Adding locales as there is language related issue with ondrej repository.
-      apt-get install apt-transport-https lsb-release ca-certificates locales locales-all -y
-      export LC_ALL=en_US.UTF-8
-      export LANG=en_US.UTF-8
       wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
       echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
       apt-get update
